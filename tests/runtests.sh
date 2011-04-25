5e184e52c1ffc6a5ec413aa044d15aec46ef3ca4
Michal Novotny <mignov@gmail.com>
2011-04-25 16:16:40 +0200

    Add test for binary file {en|de}cryption
    Signed-off-by: Michal Novotny <mignov@gmail.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 php test-complex.phpt				|| exit 1
-#./test-binary.sh				|| exit 1
+./test-binary.sh				|| exit 1
 ./test-fail.sh					|| exit 1
 
 echo "All tests passed successfully"
